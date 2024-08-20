51afe95b239241c38f1ead95520339d1ad8081d0
Yu Zhengwen <yuzhengwen0217@gmail.com>
2024-08-20 17:46:48 +0800

    Changed cursor icon

---
+++ Assets/Scripts/SetCursor.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetCursor : MonoBehaviour
+{
+    public Texture2D cursorTexture;
+
+    void Start()
+    {
+
+        //set the cursor origin to its centre. (default is upper left corner)
+        Vector2 cursorOffset = new Vector2(cursorTexture.width / 2, cursorTexture.height / 2);
+
+        //Sets the cursor to the Crosshair sprite with given offset 
+        //and automatic switching to hardware default if necessary
+        Cursor.SetCursor(cursorTexture, cursorOffset, CursorMode.Auto);
+    }
+}
